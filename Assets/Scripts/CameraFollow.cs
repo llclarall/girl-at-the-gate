5c4d2c46a117242ecb749390e923de1591ecc593
Clara Moubarak <clara.moubarak@edu.univ-eiffel.fr>
2026-02-12 12:19:37 +0100

    update cabin scene
    - added camera bounds so to not see outside of the cabin (script CameraFollow)
    - imported assets to add elements of decoration

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,14 +4,18 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public Transform player; 
-    private Vector3 offset; 
+    public Transform player;
+    private Vector3 offset;
+    private float maxCameraX = 8.4f + 5f; // limit to the right of the cabin so the camera doesn't show outside of the cabin
+    private float minCameraX = -3f + 5f; // limit to the left of the cabin
 
     void Start()
     {
         if (player != null)
         {
             offset = transform.position - player.position;
+            maxCameraX = 8.4f + offset.x;
+            minCameraX = -3f + offset.x;
         }
     }
 
@@ -19,8 +23,9 @@ public class CameraFollow : MonoBehaviour
     {
         if (player == null) return;
 
-        // Calculate the new target position    
         Vector3 targetPosition = new Vector3(player.position.x + offset.x, transform.position.y, player.position.z + offset.z);
+
+        targetPosition.x = Mathf.Max(Mathf.Min(targetPosition.x, maxCameraX), minCameraX);
 
         transform.position = targetPosition;
     }
