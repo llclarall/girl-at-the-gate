18d6f4637fc7faaaec57c9acf16b416b6f1ed3c0
Clara Moubarak <clara.moubarak@edu.univ-eiffel.fr>
2026-03-29 13:18:05 +0200

    cemetery scene upadted
    added the door to access the underground scene

--- Assets/Scripts/OscInputRuntimeBridge.cs
+++ Assets/Scripts/OscInputRuntimeBridge.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Concurrent;
-using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
@@ -15,6 +14,11 @@ public class OscInputRuntimeBridge : MonoBehaviour
     [Header("OSC Runtime Bridge")]
     [SerializeField] private int listenPort = 9000;
     [SerializeField] private bool verboseLogs;
+    [SerializeField] private float axisDeadZone = 0.08f;
+    [SerializeField] private bool invertVerticalAxis;
+    [SerializeField] private bool axisUsesZeroToOneRange;
+    [SerializeField] private bool autoStopWhenAxisIdle = true;
+    [SerializeField] private float axisIdleTimeout = 0.2f;
 
     private readonly ConcurrentQueue<OscPacket> _queue = new();
     private UdpClient _udp;
@@ -23,6 +27,12 @@ public class OscInputRuntimeBridge : MonoBehaviour
 
     private StarterAssetsInputs _starterInputs;
     private PlayerInteraction _playerInteraction;
+    private float _lastHorizontalAxisMessageTime = -999f;
+    private float _lastVerticalAxisMessageTime = -999f;
+    private bool _horizontalForcedToZero;
+    private bool _verticalForcedToZero;
+    private float _lastHorizontalAxisValue;
+    private float _lastVerticalAxisValue;
 
     private struct OscPacket
     {
@@ -64,6 +74,8 @@ public class OscInputRuntimeBridge : MonoBehaviour
         {
             DispatchPacket(packet);
         }
+
+        ApplyAxisIdleStop();
     }
 
     private void ResolveTargetsIfNeeded()
@@ -221,46 +233,128 @@ public class OscInputRuntimeBridge : MonoBehaviour
             return true;
         }
 
-        char argType = typeTag[1];
         packet.Address = address;
 
-        switch (argType)
+        bool hasNumericValue = false;
+        float lastNumericValue = 0f;
+
+        for (int i = 1; i < typeTag.Length; i++)
         {
-            case 'f':
-                if (cursor + 4 > start + length)
-                {
-                    return false;
-                }
+            char argType = typeTag[i];
 
-                packet.Value = ReadFloat(data, cursor);
-                packet.HasValue = true;
-                return true;
+            switch (argType)
+            {
+                case 'f':
+                    if (cursor + 4 > start + length)
+                    {
+                        return false;
+                    }
 
-            case 'i':
-                if (cursor + 4 > start + length)
-                {
-                    return false;
-                }
+                    lastNumericValue = ReadFloat(data, cursor);
+                    cursor += 4;
+                    hasNumericValue = true;
+                    break;
 
-                packet.Value = ReadInt(data, cursor);
-                packet.HasValue = true;
-                return true;
+                case 'd':
+                    if (cursor + 8 > start + length)
+                    {
+                        return false;
+                    }
 
-            case 'T':
-                packet.Value = 1f;
-                packet.HasValue = true;
-                return true;
+                    lastNumericValue = (float)ReadDouble(data, cursor);
+                    cursor += 8;
+                    hasNumericValue = true;
+                    break;
 
-            case 'F':
-                packet.Value = 0f;
-                packet.HasValue = true;
-                return true;
+                case 'i':
+                    if (cursor + 4 > start + length)
+                    {
+                        return false;
+                    }
 
-            default:
-                packet.Value = 1f;
-                packet.HasValue = false;
-                return true;
+                    lastNumericValue = ReadInt(data, cursor);
+                    cursor += 4;
+                    hasNumericValue = true;
+                    break;
+
+                case 'h':
+                case 't':
+                    if (cursor + 8 > start + length)
+                    {
+                        return false;
+                    }
+
+                    lastNumericValue = ReadInt64(data, cursor);
+                    cursor += 8;
+                    hasNumericValue = true;
+                    break;
+
+                case 'T':
+                    lastNumericValue = 1f;
+                    hasNumericValue = true;
+                    break;
+
+                case 'F':
+                    lastNumericValue = 0f;
+                    hasNumericValue = true;
+                    break;
+
+                case 's':
+                    {
+                        string stringValue = ReadPaddedString(data, ref cursor, start + length);
+                        if (float.TryParse(stringValue, out float parsed))
+                        {
+                            lastNumericValue = parsed;
+                            hasNumericValue = true;
+                        }
+                    }
+                    break;
+
+                case 'b':
+                    if (cursor + 4 > start + length)
+                    {
+                        return false;
+                    }
+
+                    int blobSize = ReadInt(data, cursor);
+                    cursor += 4;
+                    if (blobSize < 0 || cursor + blobSize > start + length)
+                    {
+                        return false;
+                    }
+
+                    cursor += blobSize;
+                    while (cursor % 4 != 0 && cursor < start + length)
+                    {
+                        cursor++;
+                    }
+                    break;
+
+                case 'c':
+                case 'r':
+                case 'm':
+                    if (cursor + 4 > start + length)
+                    {
+                        return false;
+                    }
+
+                    cursor += 4;
+                    break;
+
+                case 'N':
+                case 'I':
+                    break;
+
+                default:
+                    packet.Value = 1f;
+                    packet.HasValue = false;
+                    return true;
+            }
         }
+
+        packet.Value = hasNumericValue ? lastNumericValue : 1f;
+        packet.HasValue = hasNumericValue;
+        return true;
     }
 
     private void DispatchPacket(OscPacket packet)
@@ -270,6 +364,8 @@ public class OscInputRuntimeBridge : MonoBehaviour
         {
             return;
         }
+
+        address = address.ToLowerInvariant();
 
         float value = packet.HasValue ? packet.Value : 1f;
 
@@ -315,30 +411,114 @@ public class OscInputRuntimeBridge : MonoBehaviour
                 break;
 
             case "/droite":
+            case "/doite":
                 if (_starterInputs != null)
                 {
-                    _starterInputs.OnOscMoveRight();
+                    if (packet.HasValue)
+                    {
+                        float horizontal = AxisFromPositive(value);
+                        _starterInputs.OnOscMoveHorizontal(horizontal);
+                        _lastHorizontalAxisValue = horizontal;
+                    }
+                    else
+                    {
+                        _starterInputs.OnOscMoveRight();
+                        _lastHorizontalAxisValue = 1f;
+                    }
+
+                    MarkHorizontalAxisUpdate();
                 }
                 break;
 
             case "/gauche":
                 if (_starterInputs != null)
                 {
-                    _starterInputs.OnOscMoveLeft();
+                    if (packet.HasValue)
+                    {
+                        float horizontal = AxisFromNegative(value);
+                        _starterInputs.OnOscMoveHorizontal(horizontal);
+                        _lastHorizontalAxisValue = horizontal;
+                    }
+                    else
+                    {
+                        _starterInputs.OnOscMoveLeft();
+                        _lastHorizontalAxisValue = -1f;
+                    }
+
+                    MarkHorizontalAxisUpdate();
                 }
                 break;
 
             case "/avancer":
+            case "/devant":
                 if (_starterInputs != null)
                 {
-                    _starterInputs.OnOscMoveForward();
+                    if (packet.HasValue)
+                    {
+                        float vertical = AxisFromPositive(value);
+                        _starterInputs.OnOscMoveVertical(vertical);
+                        _lastVerticalAxisValue = vertical;
+                    }
+                    else
+                    {
+                        _starterInputs.OnOscMoveForward();
+                        _lastVerticalAxisValue = 1f;
+                    }
+
+                    MarkVerticalAxisUpdate();
                 }
                 break;
 
             case "/reculer":
+            case "/derriere":
                 if (_starterInputs != null)
                 {
-                    _starterInputs.OnOscMoveBackward();
+                    if (packet.HasValue)
+                    {
+                        float vertical = AxisFromNegative(value);
+                        _starterInputs.OnOscMoveVertical(vertical);
+                        _lastVerticalAxisValue = vertical;
+                    }
+                    else
+                    {
+                        _starterInputs.OnOscMoveBackward();
+                        _lastVerticalAxisValue = -1f;
+                    }
+
+                    MarkVerticalAxisUpdate();
+                }
+                break;
+
+            case "/axis1":
+            case "/axe1":
+            case "/x":
+            case "/movex":
+            case "/horizontal":
+            case "/droite-gauche":
+            case "/droite_gauche":
+                if (_starterInputs != null)
+                {
+                    float horizontal = packet.HasValue ? NormalizeAxis(value) : 0f;
+                    _starterInputs.OnOscMoveHorizontal(horizontal);
+                    _lastHorizontalAxisValue = horizontal;
+                    MarkHorizontalAxisUpdate();
+                }
+                break;
+
+            case "/axis2":
+            case "/axe2":
+            case "/y":
+            case "/movey":
+            case "/vertical":
+            case "/avant-arriere":
+            case "/avant_arriere":
+                if (_starterInputs != null)
+                {
+                    float vertical = packet.HasValue ? NormalizeAxis(value) : 0f;
+                    vertical = invertVerticalAxis ? -vertical : vertical;
+                    _starterInputs.OnOscMoveVertical(vertical);
+                    _lastVerticalAxisValue = vertical;
+                    MarkVerticalAxisUpdate();
                 }
                 break;
 
@@ -346,9 +526,75 @@ public class OscInputRuntimeBridge : MonoBehaviour
                 if (_starterInputs != null)
                 {
                     _starterInputs.OnOscMoveStop();
+                    _lastHorizontalAxisValue = 0f;
+                    _lastVerticalAxisValue = 0f;
+                    MarkHorizontalAxisUpdate();
+                    MarkVerticalAxisUpdate();
                 }
                 break;
         }
+    }
+
+    private void ApplyAxisIdleStop()
+    {
+        if (!autoStopWhenAxisIdle || _starterInputs == null)
+        {
+            return;
+        }
+
+        float now = Time.unscaledTime;
+
+        if (now - _lastHorizontalAxisMessageTime > axisIdleTimeout)
+        {
+            if (!_horizontalForcedToZero && Mathf.Abs(_lastHorizontalAxisValue) <= axisDeadZone)
+            {
+                _starterInputs.OnOscMoveHorizontal(0f);
+                _horizontalForcedToZero = true;
+            }
+        }
+
+        if (now - _lastVerticalAxisMessageTime > axisIdleTimeout)
+        {
+            if (!_verticalForcedToZero && Mathf.Abs(_lastVerticalAxisValue) <= axisDeadZone)
+            {
+                _starterInputs.OnOscMoveVertical(0f);
+                _verticalForcedToZero = true;
+            }
+        }
+    }
+
+    private void MarkHorizontalAxisUpdate()
+    {
+        _lastHorizontalAxisMessageTime = Time.unscaledTime;
+        _horizontalForcedToZero = false;
+    }
+
+    private void MarkVerticalAxisUpdate()
+    {
+        _lastVerticalAxisMessageTime = Time.unscaledTime;
+        _verticalForcedToZero = false;
+    }
+
+    private float NormalizeAxis(float raw)
+    {
+        if (axisUsesZeroToOneRange)
+        {
+            raw = raw * 2f - 1f;
+        }
+
+        float clamped = Mathf.Clamp(raw, -1f, 1f);
+        return Mathf.Abs(clamped) < axisDeadZone ? 0f : clamped;
+    }
+
+    private float AxisFromPositive(float raw)
+    {
+        return Mathf.Clamp01(raw) < axisDeadZone ? 0f : Mathf.Clamp01(raw);
+    }
+
+    private float AxisFromNegative(float raw)
+    {
+        float magnitude = Mathf.Clamp01(Mathf.Abs(raw));
+        return magnitude < axisDeadZone ? 0f : -magnitude;
     }
 
     private static string ReadPaddedString(byte[] data, ref int cursor, int end)
@@ -399,5 +645,39 @@ public class OscInputRuntimeBridge : MonoBehaviour
         };
 
         return BitConverter.ToSingle(bytes, 0);
+    }
+
+    private static double ReadDouble(byte[] data, int offset)
+    {
+        byte[] bytes = new byte[8]
+        {
+            data[offset + 7],
+            data[offset + 6],
+            data[offset + 5],
+            data[offset + 4],
+            data[offset + 3],
+            data[offset + 2],
+            data[offset + 1],
+            data[offset]
+        };
+
+        return BitConverter.ToDouble(bytes, 0);
+    }
+
+    private static long ReadInt64(byte[] data, int offset)
+    {
+        byte[] bytes = new byte[8]
+        {
+            data[offset + 7],
+            data[offset + 6],
+            data[offset + 5],
+            data[offset + 4],
+            data[offset + 3],
+            data[offset + 2],
+            data[offset + 1],
+            data[offset]
+        };
+
+        return BitConverter.ToInt64(bytes, 0);
     }
 }
